b272e81ec53cf9f75bfe9a9a913fd7a275cbcdde
elijahelorm <elijah.elorm25@gmail.com>
2023-03-17 15:58:25 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  2 /*
+  3  *main - using puts function to print a line of code
+  4  *
+  5  * Return: 0
+  6 */ 
+  7 int main(void)
+  8 {       
+  9         puts("\"Programming is like building a multilingual puzzle");
+ 10         return(0);
+ 11 }       
