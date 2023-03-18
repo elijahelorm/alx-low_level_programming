d9031c6ba4cd3fa46be8d574f0767e833149067f
elijahelorm <elijah.elorm25@gmail.com>
2023-03-17 19:12:11 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main -print a c program without printf and puts
+ *Return: 1
+*/
+
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, msg, sizeof(msg)-1);
+	return 1;
+}	
