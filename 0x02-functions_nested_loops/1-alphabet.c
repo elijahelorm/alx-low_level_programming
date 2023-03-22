fefd548c629bf0f4129b710ba73c8a1e4d41b86f
elijahelorm <elijah.elorm25@gmail.com>
2023-03-21 18:55:05 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet -print the apohabet in lowercase
+ * print_alphabet -print the alphabet in lowercase
 */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
